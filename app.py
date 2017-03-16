68936c4e9943f93310789e57aa6655c2970dae6b
helenejund <helene.jund@unistra.fr>
2017-03-16 16:20:47 +0100

    add docstrings

--- app.py
+++ app.py
@@ -1,9 +1,11 @@
 #!/usr/bin/env python
 
 def greetings():
+    """Salut RESIF"""
     print("Hello RESIF people")
 
 def repeat(x, callback):
+    """Call x time"""
     for _ in range(x):
         callback()
 
